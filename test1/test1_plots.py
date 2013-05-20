608755af02e85edf822499be1fb162558a53c872
tomaszkacprzak <tomasz.kacprzak.09@ucl.ac.uk>
2013-05-21 00:06:27 +0100

    test1_plots uses new titles for profiles

--- test1/test1_plots.py
+++ test1/test1_plots.py
@@ -13,48 +13,64 @@ import galsim
 import copy
 import datetime  
 
+# columns for the results file
 dtype_results   = { 'names'  : ['g1','g2'],
                     'formats': ['f4']*2 }
 
 global logger , config , args
 
-
+# figure size
 figure_size_x = 8
 figure_size_y = 6
 
+# define linespecs for methods
 lines_g1 = ['bx-','rx-','cx-']
 lines_g2 = ['b+-','rx-','cx-']
 
+# this has to change if the config file changes
+profile_names = ['Gaussian','Exponential','DeVaucouleurs','Bulge+Disc','Bulge+Disc non-coelliptical', 'Bulge+Disc non-coelliptical off-center']
+
 def savePlots():
-
-	n_angles = config['gal']['num']
-
+	"""
+	Read in the results files for all methods. Plot gdi/gi vs hlr for each pair of profile and 
+	intrinsic ellipticiticy. Put all methods results on same axis with different colors.
+	The properties of the figures are hardcoded as globals in the top of the file.
+	"""
+	# get the true shears
 	g1_true = config['eval_variables']['fg1']
 	g2_true = config['eval_variables']['fg2']
 
+	# get the ellipticity, hlr, profile and method grids
 	ellip = [(config['eval_variables']['ffirst_ellip'] + i*config['eval_variables']['fellip_step']) for i in range(config['eval_variables']['inellip'])]
 	hlr   =	[(config['eval_variables']['ffirst_hlr']   + i*config['eval_variables']['fhlr_step'])   for i in range(config['eval_variables']['inhlr'])]
 	profile = range(1,len(config['gal']['first']['items'])+1)  
 	method = range(len(args.methods))
 
+	# get the count for grids
 	n_ellip = len(ellip)
 	n_hlr = len(hlr)
 	n_profile = len(profile)
 	n_method = len(args.methods)
 
+	# initialise 4D results arrays
 	results_g1_mean = numpy.empty((n_method,n_profile,n_ellip,n_hlr)) 
 	results_g2_mean = numpy.empty((n_method,n_profile,n_ellip,n_hlr)) 
 	results_g1_stdm = numpy.empty((n_method,n_profile,n_ellip,n_hlr)) 
 	results_g2_stdm = numpy.empty((n_method,n_profile,n_ellip,n_hlr)) 
 
+	# loop over methods
 	for im,m in enumerate(args.methods):
+		# loop over profiles
 		for ip,p in enumerate(profile):
+			# loop over ellipticities
 			for ie,e in enumerate(ellip):
+				# loop over hlr
 				for ih,h in enumerate(hlr):
+					# get the filename and open file
 					filename_results = 'profile%02dellip%02dhlr%02d.meds.%s.cat' % (p, e*10, h*10, m)
 					filepath_results = os.path.join(args.dirpath_results,filename_results)
-					# print filename_results
 					results = numpy.loadtxt(filepath_results,dtype=dtype_results)
+					# get the number of galaxies in the results
 					n_gals = results.shape[0]
 
 					# the errorbars are taken from the 0 intrinsic ellipticity case, which doesn't contain shape noise
@@ -67,6 +83,7 @@ def savePlots():
 						results_g1_stdm[im,ip,ie,ih] = results_g1_stdm[im,ip,0,ih]
 						results_g2_stdm[im,ip,ie,ih] = results_g2_stdm[im,ip,0,ih]
 
+					# get the shear
 					results_g1_mean[im,ip,ie,ih] = numpy.mean(results['g1'])
 					results_g2_mean[im,ip,ie,ih] = numpy.mean(results['g2'])
 
@@ -84,17 +101,19 @@ def savePlots():
 			pylab.clf
 
 			for im in method:
+				# get the y axis as dgi/gi
 				y1 = (results_g1_mean[im,ip,ie,:] - g1_true)  / g1_true
 				y2 = (results_g2_mean[im,ip,ie,:] - g2_true)  / g2_true
 				s1 = results_g1_stdm[im,ip,ie,:] / g1_true
 				s2 = results_g2_stdm[im,ip,ie,:] / g2_true
+
+				# plot
 				pylab.errorbar(hlr,y1,yerr=s1,fmt=lines_g1[im])
 				pylab.errorbar(hlr,y2,yerr=s2,fmt=lines_g2[im])
 			
-			profile_name = config['gal']['first']['items'][ip]['type']
 			pylab.xlabel('hlr')
 			pylab.ylabel('dgi/gi')
-			pylab.title('profile %s , ellipticity %2.2f' % (profile_name,e))
+			pylab.title('%s , ellipticity %2.2f' % (profile_names[ip],e))
 			legend = [val for val in args.methods for _ in (0, 1)]
 			pylab.legend(legend)
 			filename_fig = 'fig.profile%02dellip%02d.png' % (ip,e*10)
@@ -114,7 +133,11 @@ def savePlots():
 if __name__ == "__main__":
 
     
-    description = 'Compare reconvolved and directly created galaxies.'
+    description = "Save plots for the results of test1. Assumes that the results have the following filenames: \
+    for meds file data/profile01ellip02hlr12.meds , for im3shape method the results are dirpath_results/profile01ellip02hlr12.meds.im3shape.cat. \
+    The results from all methods will be put on one plot, you can specify the methods names using --methods argument. \
+    example usage: test1_plots.py  --methods im3shape hsm --dirpath_results ./results --filepath_config ./shear_test1.meds.yaml"
+
 
     # parse arguments
     parser = argparse.ArgumentParser(description=description, add_help=True)
@@ -135,4 +158,5 @@ if __name__ == "__main__":
     # load the configuration file
     config = yaml.load(open(args.filepath_config,'r')) 
 
+    # run the main function
     savePlots()
