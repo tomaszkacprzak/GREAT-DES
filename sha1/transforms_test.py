8080f83ee08b86c15da4c2ee4b67c41df100066c
tomaszkacprzak <tomasz.kacprzak.09@ucl.ac.uk>
2013-09-25 15:17:05 +0100

    added PSF

--- sha1/transforms_test.py
+++ sha1/transforms_test.py
@@ -186,7 +186,7 @@ def get_image(gsgal,gspsf=None,n_pix=100,center=False):
 
 
 
-def get_multiexp_object(transforms,truth):
+def get_multiexp_object():
     """
     Save MultiExposureObject to a meds file. 
     First create the coadd with pixel size = 0.27, using the galaxy parameters in the config file.
@@ -196,6 +196,9 @@ def get_multiexp_object(transforms,truth):
     """
 
     # lists init
+
+    truth = config['truth']
+    transforms = config['transforms']
 
     list_images = []
     list_wcstrans = []
@@ -218,7 +221,7 @@ def get_multiexp_object(transforms,truth):
     list_images += [img]
     list_wcstrans += [wcstr]
 
-    psf = galsim.Moffat(beta=3.5,fwhm=0.9) 
+    psf = galsim.Moffat(beta=truth['psf_beta'],fwhm=truth['psf_fwhm']) 
 
     for itr,tr in enumerate(transforms):
         
@@ -243,7 +246,7 @@ def get_multiexp_object(transforms,truth):
 
         list_wcstrans += [wcstr]
         list_images += [img]
-        
+
 
     # create MultiExposureObject
 
@@ -251,6 +254,96 @@ def get_multiexp_object(transforms,truth):
 
     print meo
     galsim.des.write_meds(args.filename_output,[meo])
+
+
+def save_psf_images():
+
+    # this script gets the PSF for shear test 1
+    # two resolutons : same as galaxy, saved in psf.fits, upsampled and padded, saved in psf.hires.fits
+
+    import galsim
+    import yaml
+    import numpy
+
+    # PSF at resolution of the galaxy
+    logger.info('getting single PSF at the pixel scale of a galaxy')
+
+    n_pix = config['truth']['n_pix']
+    pixel_scale = DES_PIXEL_SCALE
+
+    psf_type = galsim.Moffat
+    psf_fwhm = config['truth']['psf_fwhm']
+    psf_beta = config['truth']['psf_beta']
+
+    psf = psf_type(fwhm=psf_fwhm,beta=psf_beta)
+    pix = galsim.Pixel(xw=pixel_scale)
+    img = galsim.ImageD(n_pix,n_pix)
+    final = galsim.Convolve([psf,pix])
+    dx = (numpy.random.random() - 0.5)*0.5*pixel_scale
+    dy = (numpy.random.random() - 0.5)*0.5*pixel_scale
+    final.applyShift(dx=dx,dy=dy)
+    final.draw(img,dx=pixel_scale)
+
+    filename_psf = 'psf.single.%dx%d.fits' % (config['truth']['n_pix'],config['truth']['n_pix'])
+    img.write(filename_psf)
+    print 'saved %s' % filename_psf
+
+    logger.info('getting single PSF at high resolution')
+    # now the hires PSF, centered in the middle
+
+    n_sub = 5
+    n_pad = 4
+    n_pix_hires = (config['truth']['n_pix'] + n_pad) * n_sub
+    pixel_scale_hires = float(DES_PIXEL_SCALE) / float(n_sub)
+
+    psf = psf_type(fwhm=psf_fwhm,beta=psf_beta)
+    pix = galsim.Pixel(xw=pixel_scale)
+    img = galsim.ImageD(n_pix_hires,n_pix_hires)
+    dx = 0.5*pixel_scale
+    dy = 0.5*pixel_scale
+    final = galsim.Convolve([psf,pix])
+    final.applyShift(dx=dx,dy=dy)
+    final.draw(img,dx=pixel_scale_hires)
+
+    filename_psf_hires = 'psf.single.%dx%d.fits' % (n_pix_hires,n_pix_hires)
+    img.write(filename_psf_hires)
+    print 'saved %s' % filename_psf_hires
+
+    logger.info('getting low res PSF in a field')
+
+    # now create the PSF - fields for the shapelet pipeline
+    ny_tiles = nx_tiles = 30
+
+    psf_image = galsim.ImageF(n_pix * nx_tiles-1 , n_pix * ny_tiles-1)
+    psf_image.setScale(pixel_scale)
+    random_seed=123123
+
+    shift_radius = pixel_scale *0.5     
+    shift_radius_sq = shift_radius**2
+
+    k=0;
+    for iy in range(ny_tiles):
+        for ix in range(nx_tiles):
+            ud = galsim.UniformDeviate(random_seed+k)
+
+            # Apply a random shift_radius:
+            rsq = 2 * shift_radius_sq
+            while (rsq > shift_radius_sq):
+                dx = (2*ud()-1) * shift_radius
+                dy = (2*ud()-1) * shift_radius
+                rsq = dx**2 + dy**2
+
+            this_psf = final.createShifted(dx,dy)
+
+            b = galsim.BoundsI(ix*n_pix+1 , (ix+1)*n_pix-1, iy*n_pix+1 , (iy+1)*n_pix-1)
+            sub_psf_image = psf_image[b]
+            this_psf.draw(sub_psf_image,dx=pixel_scale)
+            k+=1;
+
+    # save the tiled PSF image
+    filename_psf_field = 'psf.field.%dx%d.fits' % (config['truth']['n_pix'],config['truth']['n_pix'])
+    psf_image.write(filename_psf_field)
+    print 'saved %s' % filename_psf_field
 
 
 
@@ -276,7 +369,7 @@ def main():
     In DS9 they will look the same as DES SE images. '
     parser = argparse.ArgumentParser(description=description, add_help=True)
     parser.add_argument('-v', '--verbosity', type=int, action='store', default=2, choices=(0, 1, 2, 3 ), help='integer verbosity level: min=0, max=3 [default=2]')
-    parser.add_argument('-o', '--filename_output', default='wcs_test_card_01.meds',type=str, action='store', help='name of the output meds file')
+    parser.add_argument('-o', '--filename_output', default='wcs_test_card_01.meds.fits',type=str, action='store', help='name of the output meds file')
     parser.add_argument('-c', '--filename_config', default='transforms_list.yaml',type=str, action='store', help='name of the yaml config file')
     
     args = parser.parse_args()
@@ -291,10 +384,10 @@ def main():
     logger.setLevel(logging_level)
 
 
-    transforms = yaml.load(open(args.filename_config))['transforms_xy']
-    truth = yaml.load(open(args.filename_config))['truth']
+    config = yaml.load(open(args.filename_config))
 
-    get_multiexp_object(transforms,truth)
+    get_multiexp_object()
+    save_psf_images()
 
 
 main()
