211c44dd7802e78b3a50ef7d5906295a8dd06bad
tomaszkacprzak <tomasz.kacprzak@phys.ethz.ch>
2015-03-12 13:46:00 +0100

    added fix hdus

--- ufig/fix_hdus.py
+++ ufig/fix_hdus.py
@@ -1,20 +1,21 @@
-import os
-import pyfits
-import logging
-import sys
-import numpy
-import sys
-import math
-import argparse
-import yaml
-import copy
+import os, pyfits, logging, sys, numpy, sys, math, argparse, yaml, copy
+
+logging_level = logging.INFO
+logger = logging.getLogger("fix_hdus") 
+logger.setLevel(logging_level)  
+log_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s   %(message)s", "%Y-%m-%d %H:%M:%S")
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(log_formatter)
+logger.addHandler(stream_handler)
+logger.propagate = False
+
 
 # ufig
 HDU_UFIG_IMAGE = 0
 # coadd
-COADD_HDU     = 2; # that is for image 
+COADD_HDU     = 1; # that is for image 
+COADD_SEG_HDU = 2;
 COADD_WT_HDU  = 3;
-COADD_SEG_HDU = 1;
 # single exp
 SE_HDU        = COADD_HDU; # that is for image
 SE_WT_HDU     = COADD_WT_HDU;
@@ -41,15 +42,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Parse the integer verbosity level from the command line args into a logging_level string
-    logging_levels = { 0: logging.CRITICAL, 
-                       1: logging.WARNING,
-                       2: logging.INFO,
-                       3: logging.DEBUG }
-    logging_level = logging_levels[args.verbosity]
-    logging.basicConfig(format="%(message)s", level=logging_level, stream=sys.stdout)
-    global logger
-    logger = logging.getLogger("add_weight") 
-    config_logging_level = logging_levels[args.verbosity - 1]
+    logging_levels = { 0: logging.CRITICAL, 1: logging.WARNING, 2: logging.INFO, 3: logging.DEBUG }; logging_level = logging_levels[args.verbosity]; logger.setLevel(logging_level)
     
     hdus_ufig = pyfits.open(args.filepath_ufig)
     logger.debug(hdus_ufig)
@@ -69,15 +62,41 @@ if __name__ == "__main__":
 
     # get the output hdus, the convention for the hdu indices in settings iterates from 1, and in pyfits from 0, so subtract one
     out_list = [None]*NHDU_OUT
-    out_list[0] = pyfits.PrimaryHDU(header=hdus_ufig[0].header)
-    out_list[COADD_HDU-1]      = pyfits.ImageHDU(data=image,header=hdus_ufig[HDU_UFIG_IMAGE].header)
+    # out_list[0] = pyfits.PrimaryHDU(header=hdus_ufig[0].header)
+    out_list[COADD_HDU-1]      = pyfits.PrimaryHDU(data=image,header=hdus_ufig[HDU_UFIG_IMAGE].header)
     out_list[COADD_SEG_HDU-1]  = pyfits.ImageHDU(data=seg,header=hdus_ufig[HDU_UFIG_IMAGE].header)
     out_list[COADD_WT_HDU-1]   = pyfits.ImageHDU(data=weight,header=hdus_ufig[HDU_UFIG_IMAGE].header)
     out_list[SE_BADPIX_HDU-1]  = pyfits.ImageHDU(data=badpix,header=hdus_ufig[HDU_UFIG_IMAGE].header)
 
     hdulist_out = pyfits.HDUList(out_list)
+    logger.info(str(out_list))
+    logger.info(str(hdulist_out))
     logger.info('saving..')
     hdulist_out.writeto(args.filepath_out,clobber=True)
     logger.info('saved %s' % args.filepath_out)
+
+    import numpy as np
+    filename_medscat = 'DES0441-4414_ufig.medscat'
+    medscat = np.loadtxt(filename_medscat)
+    n_gals = len(medscat)
+    f=open(filename_medscat,'w')
+    for il in range(n_gals):
+        f.write('%d %f %f %f %f %d\n' % (il, medscat[il,0], medscat[il,1], medscat[il,2], medscat[il,3], medscat[il,4]))
+    f.close()
+    
+    logger.info('added an index column to %s' % filename_medscat )
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
